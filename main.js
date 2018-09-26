7ac09629af57a85bcd811735c5c4ec2a2b9cd349
Sean McQuaid <sean.mcquaid2@verizon.net>
2018-09-25 22:16:29 -0400

    Added the copyright icon, fixed spacing in my array, working on logic

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 // Declare array of quotes that won't change
-const quotes = ["“You know what gets my dick hard? Helping out my friends.” — Derek", "“You and your mom are hillbillies. This is a house of learned doctors.” — Dale", "“Dad, what are you doing? It’s ‘Shark Week’!” — Dale”", "“Brennan, that is the voice of an angel. Brennan I can’t even make eye contact with you right now. Your voice is like a combination of Fergie and Jesus.” — Dale", "“Did we just become best friends?” — Brennan"];
-
-// on click listener
-
-$(document).ready(function() {
-  $("#quote-button").on("click", function(){
-    alert("IT WORKS!");
-  });
-  });
-
-// loop through array and print a single quote on each click
- 
-// remove p tag content
-
-// add p tag content from array
-
+const quotes = [
+  "“You know what gets my dick hard? Helping out my friends.” — Derek", 
+  "“You and your mom are hillbillies. This is a house of learned doctors.” — Dale", 
+  "“Dad, what are you doing? It’s ‘Shark Week’!” — Dale”", 
+  "“Brennan, that is the voice of an angel. Brennan I can’t even make eye contact with you right now. Your voice is like a combination of Fergie and Jesus.” — Dale", 
+  "“Did we just become best friends?” — Brennan"];
+  
+  // create variable with equation for random number
+  
+  let newQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  
+  // create on click listener that generates new quote everytime 
+  
+  $(document).ready(function() {
+    $("#quote-button").on("click", function(){
+        console.log(newQuote);
+    });
+    });
